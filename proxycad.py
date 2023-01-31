01565567fd514238489452a7ab1a4250f917dad6
Landry Breuil <breuil@craig.fr>
2023-01-31 16:58:55 +0100

    call init_app when launched by gunicorn

--- proxycad.py
+++ proxycad.py
@@ -281,3 +281,4 @@ else:
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+    init_app(app)
